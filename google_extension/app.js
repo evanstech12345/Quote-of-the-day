f90ceb22c1dbed7b1b1f16d9f16374f427bade12
evanstech12345 <evanshcode@gmail.com>
2023-04-11 19:41:28 -0400

    the project is ready to be deployed

--- google_extension/app.js
+++ google_extension/app.js
@@ -1,29 +1,38 @@
-const quote = document.getElementById(".quote")
+const quote = document.getElementById("quote")
 
-if(quote) {
-  quote.textContent = "quote";//changing the content of h1 using dom
-}
 // so i can use the api
 //api that gives you the quote of the day
+// let api = fetch('secrets.json')
+//   .then(response => response.json())
+//   .then(data => {
+//     const apiKey = data.api_key;
+//     // Use the apiKey variable to make API calls
+//   });
 // const category = 'happiness'
 
 //middleware
 
 
-const category = 'happiness'
-const $ = jQuery;
-$.ajax({
-  method: 'GET',
-  url: 'https://api.api-ninjas.com/v1/quotes?category=' + category,
-  headers: { 'X-Api-Key': apiKey},
-  contentType: 'application/json',
-  success: function(result) {
-    console.log(result);
-  },
-  error: function ajaxError(jqXHR) {
-    console.error('Error: ', jqXHR.responseText);
-  }
-});
+fetch('secrets.json')
+  .then(response => response.json())
+  .then(data => {
+    const apiKey = data.api_key;
+    const category = 'inspirational';
+    const $ = jQuery;
+    $.ajax({
+    method: 'GET',
+    url: 'https://api.api-ninjas.com/v1/quotes?category=' + category,
+    headers: { 'X-Api-Key': apiKey},
+    contentType: 'application/json',
+    success: function(result) {
+      console.log(result);
+      quote.textContent = JSON.stringify(result[0].quote);
+    },
+    error: function ajaxError(jqXHR) {
+      console.error('Error: ', jqXHR.responseText);
+      }
+    })
+  })
 
 // axios({
 //   method: "get",
